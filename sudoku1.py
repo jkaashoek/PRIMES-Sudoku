f2b47498c5c63eac5d18b7b3e95f66b9be27e6e4
Justin H. Kaashoek <jkaashoek@Justins-MacBook-Pro.local>
2014-02-18 17:58:21 -0500

    Added comments

--- sudoku1.py
+++ sudoku1.py
@@ -54,39 +54,31 @@ else:
 
 
 def emptySquare(board):
-    print "here"
     vals_tried = []
     i = random.randint(0, 8) # random row
     j = random.randint(0, 8) # random column
     while (str(board[i][j]) == str(0)) or ((i,j) in vals_tried):
-        print "now im here"
-        print "i and j", i,j, "i j on board", board[i][j], "values tried", vals_tried
-        print board[i][j] == 0, (i,j) in vals_tried
+        # The box being emptied must not be 0 and should not have been tried already
         i = random.randint(0, 8) # random row
         j = random.randint(0, 8) # random column
-    val_now = board[i][j]
+    val_now = board[i][j] # value before the box changes to 0
     board[i][j] =  0 # set cell to 0
-    vals_tried.append((i,j))
+    vals_tried.append((i,j)) # Now that this value has been tried, it does not have to be tried again
     F = createSudoku(board) # Make a new sudoku with our new constraints
     while (not exactly_one_model(F)):
-        print "hello"
-         # We know the resulting sudoku is not unique
-        print "num not empty:", notEmpty(board), "len values tried", len(vals_tried)
+        # After the square is emptied, the resulting sudoku must have a unique solution
         if notEmpty(board) == len(vals_tried):
+            # If every value on the board has been emptied and all of them have not worked, no squares can be emptied and we can stop looking
             break
         i = random.randint(0, 8) # random row
         j = random.randint(0, 8) # random column
         while (str(board[i][j]) == str(0)) or ((i,j) in vals_tried):
-            print "is it in values tried", (i,j) in vals_tried
-            print "values tried still adding toooo:::::", vals_tried
-            print "and now here"
             i = random.randint(0, 8) # random row
             j = random.randint(0, 8) # random column
-        val_now = board[i][j]
+        val_now = board[i][j] # value before the box changes to 0
         board[i][j] =  0 # set cell to 0
-        vals_tried.append((i,j))
+        vals_tried.append((i,j)) # Now that this value has been tried, it does not have to be tried again
         F = createSudoku(board) # Make a new sudoku with our new constraints
-    print "values tried", vals_tried
     return board, vals_tried
 
 def createSudoku(board):
@@ -98,6 +90,7 @@ def createSudoku(board):
     cols_distinct = [Distinct([X[i][j] for i in range(9)]) for j in range(9)]
     # Every 3 x 3 square should be disntinct
     three_by_three_distinct = [ Distinct([X[3*k + i][3*l + j] for i in range(3) for j in range(3)]) for k in range(3) for l in range(3)]
+    # There are values already set in the board, which we need to take into account
     already_set = []
     for i in range(9):
         for j in range(9):
@@ -118,19 +111,15 @@ def notEmpty(board):
 num_rand = 80 # number of unfilled cells in sudoku puzzle
 vals_tried = [] # which box we have tried to change but they failed. A box value fails if we change it to empty and the resulting sudoku is not unique.
 while (num_rand >= 0):
-    print "LOOP NUMBER:", num_rand
     board_after, vals_tried = emptySquare(r)
-    print "vals tried in while loop: ", vals_tried
-    print "num not empty:", notEmpty(board_after)
     if notEmpty(board_after) == len(vals_tried):
             break
     num_rand -= 1
     r = board_after
     print_matrix(r)
     
-        #print_matrix(r)
-
-print "here", print_matrix(r)
+        
+print_matrix(r)
 
 ## instance_c = [ If(r[i][j] == 0, 
 ##                   True, 
