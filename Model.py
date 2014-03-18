0245440fe26d758827edab128d7c5a1267a5ea4c
amychou <amychou124@gmail.com>
2014-03-18 14:00:57 -0600

    Create Model.py

---
+++ Model.py
@@ -0,0 +1,55 @@
+from z3 import *
+    
+def get_models(F, M):
+    result = []
+    s = Solver()
+    s.add(F)
+    # print "This is the solver", s
+    while True:
+        if s.check() == sat:
+            m = s.model()
+            result.append(m)
+            # Create a new constraint the blocks the current model
+            block = []
+            for d in m:
+                c = d() # convert z3 key into z3 variable
+                block.append(c != m[d])
+            s.add(Or(block))
+        else:
+            return result
+
+
+def exactly_n_models(F, n):
+    return len(get_models(F, n+1)) == n
+
+def fewer_than_n_models(F, n):
+    return len(get_models(F, n+1)) <= n
+
+def at_least_one_model(F, n):
+    return len(get_models(F, n+1)) > 0
+
+def unique(F):
+    return exactly_n_models(F, 1)
+
+def modelLength(F):
+    return len(get_models(F, 30))
+
+
+def createSudoku(board):
+    X = [[Int('x%d%d' % (i,j)) for i in range(9)] for j in range(9)]
+    valid_values = [And ( X[i][j] >= 1, X[i][j] <= 9) for i in range(9) for j in range(9)]
+    # Every row should be disntinct
+    row_distinct = [Distinct(X[i]) for i in range(9)]
+    # Every column should be disntinct
+    cols_distinct = [Distinct([X[i][j] for i in range(9)]) for j in range(9)]
+    # Every 3 x 3 square should be disntinct
+    three_by_three_distinct = [ Distinct([X[3*k + i][3*l + j] for i in range(3) for j in range(3)]) for k in range(3) for l in range(3)]
+    # There are values already set in the board, which we need to take into account
+    already_set = []
+    for i in range(9):
+        for j in range(9):
+            if board[i][j] != 0:
+                already_set.append(X[i][j] == board[i][j])
+
+    F = valid_values + row_distinct + cols_distinct + three_by_three_distinct + already_set
+    return F
