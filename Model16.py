f46da323736cf617e4765192fd753d33400b05e9
Justin H. Kaashoek <jkaashoek@Justins-MacBook-Pro-2.local>
2014-05-08 06:54:11 -0400

    Added code for generating 16 x 16 puzzle

---
+++ Model16.py
@@ -0,0 +1,67 @@
+from z3 import *
+import time
+    
+def get_models(F, M):
+    result = []
+    s = Solver()
+    s.add(F)
+    # print "This is the solver", s
+    while True:
+        if s.check() == sat:
+            m = s.model()
+            result.append(m)
+            # Create a new constraint the blocks the current model
+            block = []
+            for d in m:
+                c = d() # convert z3 key into z3 variable
+                block.append(c != m[d])
+            s.add(Or(block))
+        else:
+            return result
+
+
+def exactly_n_models(F, n):
+    return len(get_models(F, n+1)) == n
+
+def fewer_than_n_models(F, n):
+    return len(get_models(F, n+1)) <= n
+
+def at_least_one_model(F, n):
+    return len(get_models(F, n+1)) > 0
+
+def unique(F):
+    return exactly_n_models(F, 1)
+
+def modelLength(F):
+    return len(get_models(F, 30))
+
+
+def createSudoku(board):
+    X = [[Int('x%d%d' % (i,j)) for i in range(16)] for j in range(16)]
+    valid_values = [And ( X[i][j] >= 1, X[i][j] <= 16) for i in range(16) for j in range(16)]
+    # Every row should be disntinct
+    row_distinct = [Distinct(X[i]) for i in range(16)]
+    # Every column should be disntinct
+    cols_distinct = [Distinct([X[i][j] for i in range(16)]) for j in range(16)]
+    # Every 3 x 3 square should be disntinct
+    three_by_three_distinct = [ Distinct([X[4*k + i][4*l + j] for i in range(4) for j in range(4)]) for k in range(4) for l in range(4)]
+    # There are values already set in the board, which we need to take into account
+    already_set = []
+    ## for i in range(16):
+    ##     for j in range(16):
+    ##         if board[i][j] != 0:
+    ##             already_set.append(X[i][j] == board[i][j])
+    ## F = valid_values + row_distinct + cols_distinct + three_by_three_distinct + already_set
+    s = Solver()
+    s.add(valid_values + row_distinct + cols_distinct + three_by_three_distinct)
+    if s.check() == sat:
+        m = s.model()
+        r = [ [ m.evaluate(X[i][j]) for j in range(16) ] for i in range(16) ]
+    return r
+
+time = time.time()
+board = createSudoku([])
+timelater = time.time()
+print board
+print "time to run:", time-timelater
+
---
+++ Puzzle16.py
@@ -0,0 +1,79 @@
+import random
+from Generator import *
+from Model import *
+
+class Puzzle:
+
+    def __init__(self, nSolutions, nEmpty):
+        self.__fullBoard = new() #create new sudoku board from generator
+        self.__nSolutions = nSolutions #number of solutions board has
+        self.__nEmpty = nEmpty #number of squares removed
+        self.__emptiedBoard = copyListOfLists(self.__fullBoard)
+        self.__valsTried = []
+        self.__emptiedSquares = []
+        self.__numSquaresRemaining = self.__nEmpty
+        self.__continue = True
+        self.__currNumSolutions = 1
+
+    def reset(self):
+        self.__valsTried = [] #remove all values from valsTried
+
+    def getRandomIJ(self):
+        ls = []
+        for i in range(16):
+            for j in range(16):
+                if (((i, j) not in self.__emptiedSquares) and ((i, j) not in self.__valsTried)):
+                    ls.append((i, j))
+        if (len(self.__emptiedSquares) + len(self.__valsTried) == 216):
+            return -1, -1
+        else:
+            i, j = random.choice(ls)
+            return i, j
+
+    def empty(self):
+        while(self.__numSquaresRemaining>0 and self.__continue):
+            print("Emptied " + str(self.__nEmpty - self.__numSquaresRemaining ) + " squares")
+            self.removeSquare()
+            self.__numSquaresRemaining -= 1
+        print("Emptied " + str(len(self.__emptiedSquares)) + " squares")
+        print("This puzzle has " + str(self.__currNumSolutions) + " solution(s)")
+
+    def removeSquare(self):
+        i, j = self.getRandomIJ()
+        while ((not self.validSquare(i, j))):
+            if ((i, j) == (-1, -1)):
+                break
+            self.__valsTried.append((i, j))
+            print("Another value added to valsTried: " + str(len(self.__valsTried)) +
+                  " total values in valsTried and " + str(len(self.__emptiedSquares)) + " already empty.")
+            i, j = self.getRandomIJ()
+
+        if ((i, j) != (-1, -1)):
+            self.__emptiedBoard[i][j] = 0
+            self.__emptiedSquares.append((i, j))
+            self.reset()
+        else:
+            self.__continue = False
+
+    def validSquare(self, i, j):
+        #val_now = board[i][j] # value before the box changes to 0
+        tempBoard = copyListOfLists(self.__emptiedBoard)
+        tempBoard[i][j] =  0 # set cell to 0
+        F = createSudoku(tempBoard)
+        self.__currNumSolutions = len(get_models(F, self.__nSolutions + 1))
+        return (self.__currNumSolutions <= self.__nSolutions and self.__currNumSolutions > 0)
+
+    def getPuzzle(self):
+        return self.__emptiedBoard
+    
+    def getOriginalBoard(self):
+        return self.__fullBoard
+
+    def printPuzzle(self):
+        print_matrix(self.__emptiedBoard)
+
+
+    def puzzleID(self):
+        return self.__currNumSolutions, len(self.__emptiedSquares)
+
+
