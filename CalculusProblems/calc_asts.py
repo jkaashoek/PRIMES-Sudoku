579a9a06d5be3b2b13056b8927d13f8dda09bb47
Justin H. Kaashoek <jkaashoek@Justins-MacBook-Pro.local>
2015-11-03 21:52:05 -0500

    outline for generating derivative problems

---
+++ CalculusProblems/calc_asts.py
@@ -0,0 +1,20 @@
+Derivatives:
+ Introducing derivatives:
+  - rate of change, average rate of change
+    - be able to find given any f(x), a polynomial, and a starting and ending point
+   - proving derivatives a polynomial f(x) using the definition of a derivative- lim(x->h)((f(a+h) - f(a))/h)
+    - be able to accomplish this given any function:
+      - different types of problems: polynomials, square roots, rational  
+  - Different derivative rules
+    - addition
+    - subtraction
+    - multiplication
+    - power rule
+    
+ Finding derivatives:
+  - word problems (possibly use natural language processing)
+  - instantaneous velcotiy, speed, acceleration
+  - trig functions, combinations of trig functions
+    - proofs for the derivatives of different trig functions
+
+
